4ab0ffdf367b16a2ae14ccf15384c0e90d03048f
Patricija Delac <patricija.delac@agiledrop.com>
2025-01-03 14:33:23 +0100

    Set up Sass in Vite and add base styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+// https://vite.dev/config/
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// Path to the global SCSS file
+const scssPath = path.resolve(__dirname, './src/styles/lib.scss');
+
 export default defineConfig({
+  css: {
+    modules: {
+      localsConvention: 'dashesOnly', // Ensures class names follow the dashes-only convention
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "${scssPath}" as *;`,
+      },
+    },
+  },
   plugins: [react()],
 });
